4ec8d3685fee044ce7b761528c32284926ff8506
Caltonsamo19 <139620517+Caltonsamo19@users.noreply.github.com>
2025-09-06 06:51:07 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -90,8 +90,7 @@ const client = new Client({
         executablePath: undefined // Deixa o puppeteer escolher automaticamente
     },
     webVersionCache: {
-        type: 'remote',
-        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+        type: 'local'
     }
 });
 
